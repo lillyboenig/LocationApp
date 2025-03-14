01bddffec342d544b4cd485d85aabe3c02cb4e9d
lillyboenig <lilly.boenig@web.de>
2025-03-14 14:06:28 +0200

    Create LocationContext to manage and share location data across screens

---
+++ context/LocationContext.js
@@ -0,0 +1,13 @@
+import React, { createContext, useState } from 'react';
+
+export const LocationContext = createContext();
+
+export const LocationProvider = ({ children }) => {
+  const [locations, setLocations] = useState([]);
+
+  return (
+    <LocationContext.Provider value={{ locations, setLocations }}>
+      {children}
+    </LocationContext.Provider>
+  );
+};
